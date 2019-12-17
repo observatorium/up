49bd90f780f40d39ade2f70bb2d04af71cd616a4
Matthias Loibl <mail@matthiasloibl.com>
2019-12-17 19:34:26 +0100

    Make the programm exit success or error code after given duration

--- main.go
+++ main.go
@@ -26,6 +26,7 @@ import (
 	promv1 "github.com/prometheus/client_golang/api/prometheus/v1"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	dto "github.com/prometheus/client_model/go"
 	"github.com/prometheus/common/model"
 	"github.com/prometheus/prometheus/prompb"
 )
@@ -34,9 +35,11 @@ var (
 	remoteWriteRequests = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "up_remote_writes_total",
 		Help: "Total number of remote write requests.",
-	},
-		[]string{"result"},
-	)
+	}, []string{"result"})
+	queryResponses = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "up_queries_total",
+		Help: "The total number of queries made",
+	}, []string{"result"})
 	metricValueDifference = prometheus.NewHistogram(prometheus.HistogramOpts{
 		Name:    "up_metric_value_difference",
 		Help:    "The time difference of the current time stamp and the time stamp in the metrics value",
@@ -71,19 +74,22 @@ func (la *labelArg) Set(v string) error {
 		}
 		lset = append(lset, prompb.Label{Name: parts[0], Value: val})
 	}
-	*la = labelArg(lset)
+	*la = lset
 	return nil
 }
 
 func main() {
 	opts := struct {
-		EndpointWrite string
-		EndpointRead  string
-		Labels        labelArg
-		Listen        string
-		Name          string
-		Period        string
-		Token         string
+		EndpointWrite   string
+		EndpointRead    string
+		Labels          labelArg
+		Listen          string
+		Name            string
+		Token           string
+		Period          time.Duration
+		Duration        time.Duration
+		SuccessTreshold float64
+		Latency         time.Duration
 	}{}
 
 	flag.StringVar(&opts.EndpointWrite, "endpoint-write", "", "The endpoint to which to make remote-write requests.")
@@ -92,7 +98,10 @@ func main() {
 	flag.StringVar(&opts.Listen, "listen", ":8080", "The address on which internal server runs.")
 	flag.StringVar(&opts.Name, "name", "up", "The name of the metric to send in remote-write requests.")
 	flag.StringVar(&opts.Token, "token", "", "The bearer token to set in the authorization header on remote-write requests.")
-	flag.StringVar(&opts.Period, "period", "1m", "The time to wait between remote-write requests.")
+	flag.DurationVar(&opts.Period, "period", 5*time.Second, "The time to wait between remote-write requests.")
+	flag.DurationVar(&opts.Duration, "duration", 5*time.Minute, "The duration of the up command to run until it stops")
+	flag.Float64Var(&opts.SuccessTreshold, "treshold", 0.9, "The percentage of successful requests needed to succeed overall. 0 - 1")
+	flag.DurationVar(&opts.Latency, "latency", 15*time.Second, "The maximum allowable latency between writing and reading")
 	flag.Parse()
 
 	logger := log.NewLogfmtLogger(log.NewSyncWriter(os.Stderr))
@@ -110,11 +119,6 @@ func main() {
 		return
 	}
 
-	period, err := time.ParseDuration(opts.Period)
-	if err != nil {
-		level.Error(logger).Log("msg", "--period is invalid", "err", err)
-		return
-	}
 	opts.Labels = append(opts.Labels, prompb.Label{
 		Name:  "__name__",
 		Value: opts.Name,
@@ -125,8 +129,11 @@ func main() {
 		prometheus.NewGoCollector(),
 		prometheus.NewProcessCollector(prometheus.ProcessCollectorOpts{}),
 		remoteWriteRequests,
+		queryResponses,
 		metricValueDifference,
 	)
+
+	ctx := context.Background()
 
 	var g run.Group
 	{
@@ -135,10 +142,13 @@ func main() {
 		g.Add(func() error {
 			signal.Notify(sig, os.Interrupt, syscall.SIGTERM)
 			<-sig
+			level.Info(logger).Log("msg", "caught interrupt")
 			return nil
 		}, func(_ error) {
-			level.Info(logger).Log("msg", "caught interrupt")
 			close(sig)
+
+			success, errors := successerrors()
+			level.Info(logger).Log("msg", "number of requests", "success", success, "errors", errors)
 		})
 	}
 	{
@@ -165,8 +175,8 @@ func main() {
 		})
 	}
 	{
-		t := time.NewTicker(period)
-		ctx, cancel := context.WithCancel(context.Background())
+		t := time.NewTicker(opts.Period)
+		ctx, cancel := context.WithCancel(ctx)
 
 		g.Add(func() error {
 			level.Info(logger).Log("msg", "starting the remote-write client")
@@ -186,18 +196,34 @@ func main() {
 		})
 	}
 	{
-		t := time.NewTicker(period)
-		ctx, cancel := context.WithCancel(context.Background())
+		var cancel func()
+
+		if opts.Duration != 0 {
+			ctx, cancel = context.WithTimeout(ctx, opts.Duration)
+		} else {
+			ctx, cancel = context.WithCancel(ctx)
+		}
+
+		t := time.NewTicker(opts.Period)
 
 		g.Add(func() error {
 			level.Info(logger).Log("msg", "start querying for metrics")
 			for {
 				select {
 				case <-t.C:
-					if err := read(ctx, endpointRead, opts.Labels); err != nil {
+					if err := read(ctx, endpointRead, opts.Labels, opts.Latency); err != nil {
+						queryResponses.WithLabelValues("error").Inc()
 						level.Error(logger).Log("msg", "failed to query", "err", err)
+					} else {
+						queryResponses.WithLabelValues("success").Inc()
 					}
 				case <-ctx.Done():
+					success, errors := successerrors()
+					ratio := success / (success + errors)
+
+					if ratio < opts.SuccessTreshold {
+						return fmt.Errorf("failed with less than %2.f%% success ratio - actual %2.f%%", opts.SuccessTreshold*100, ratio*100)
+					}
 					return nil
 				}
 			}
@@ -212,7 +238,7 @@ func main() {
 	}
 }
 
-func read(ctx context.Context, endpoint *url.URL, labels []prompb.Label) error {
+func read(ctx context.Context, endpoint *url.URL, labels []prompb.Label, latency time.Duration) error {
 	client, err := promapi.NewClient(promapi.Config{Address: endpoint.String()})
 	if err != nil {
 		return err
@@ -241,8 +267,8 @@ func read(ctx context.Context, endpoint *url.URL, labels []prompb.Label) error {
 
 	metricValueDifference.Observe(diffSeconds)
 
-	if diffSeconds > 10 {
-		return fmt.Errorf("metric value is too old")
+	if diffSeconds > latency.Seconds() {
+		return fmt.Errorf("metric value is too old: %2.fs", diffSeconds)
 	}
 
 	return nil
@@ -298,4 +324,27 @@ func write(ctx context.Context, endpoint *url.URL, token string, wreq *prompb.Wr
 		return errors.Wrap(err, "non-200 status")
 	}
 	return nil
+}
+
+func successerrors() (float64, float64) {
+	metrics := make(chan prometheus.Metric, 2)
+	queryResponses.Collect(metrics)
+	close(metrics)
+
+	var success, errors float64
+
+	for m := range metrics {
+		m1 := &dto.Metric{}
+		_ = m.Write(m1)
+		for _, l := range m1.Label {
+			switch *l.Value {
+			case "error":
+				errors = m1.GetCounter().GetValue()
+			case "success":
+				success = m1.GetCounter().GetValue()
+			}
+		}
+	}
+
+	return success, errors
 }
