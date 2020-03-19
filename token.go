0ac2efa55f1ef8e497be04de421065c0f37cbf1f
Frederic Branczyk <fbranczyk@gmail.com>
2020-03-19 17:50:51 +0100

    Allow specifying token from file

---
+++ token.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"io/ioutil"
+	"strings"
+)
+
+func NewNoOpTokenProvider() *StaticToken {
+	return &StaticToken{token: ""}
+}
+
+type TokenProvider interface {
+	Get() (string, error)
+}
+
+type StaticToken struct {
+	token string
+}
+
+func NewStaticToken(token string) *StaticToken {
+	return &StaticToken{token: token}
+}
+
+func (t *StaticToken) Get() (string, error) {
+	return t.token, nil
+}
+
+type FileToken struct {
+	file string
+}
+
+func NewFileToken(file string) *FileToken {
+	return &FileToken{file: file}
+}
+
+func (t *FileToken) Get() (string, error) {
+	b, err := ioutil.ReadFile(t.file)
+	if err != nil {
+		return "", err
+	}
+
+	return strings.TrimSpace(string(b)), nil
+}
